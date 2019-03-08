a1e98e8062758227bda7905f04c1002dd5da9c61
Im9799 <47377166+Im9799@users.noreply.github.com>
2019-03-08 10:04:41 +0530

    Create nature.java

---
+++ nature.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num,i,sum=0;
+	Scanner s=new Scanner(System.in);
+	num=s.nextInt();
+	for(i=1;i<=num;i++)
+	{
+		sum=sum+i;
+	}
+		System.out.println(sum);
+	}
+}
