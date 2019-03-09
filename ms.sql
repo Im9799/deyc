b2870acb8a3b1e42774b5b0655eada524d694a98
Im9799 <47377166+Im9799@users.noreply.github.com>
2019-03-09 12:33:07 +0530

    Create ms.sql

---
+++ ms.sql
@@ -0,0 +1,6 @@
+create database college;
+use college;
+create table student(Register_no int(25),Name varchar(100),Dept varchar(25),Phone int(15),Native varchar(100));
+create table staff(Name varchar(100),Dept varchar(25),Phone int(15),Native varchar(100));
+show tables;
+desc student;
